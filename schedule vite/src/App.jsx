0878d16e558e8b56e2ab71ecec121f2c438488e7
Kudinov Andrej <kudinov.andrej@list.ru>
2024-02-08 19:02:19 +0300

    использовал преттир

--- schedule vite/src/App.jsx
+++ schedule vite/src/App.jsx
@@ -1,106 +1,137 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import Modal from 'react-modal';
+import Modal from 'react-modal'
 
 function App() {
-  const [name, setName] = useState("Введите Ваше имя")
-  const [lives, setLives] = useState([ ])
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [newLife, setNewLife] = useState({ name: '', background: '', force: ''});
-const [totalLivesForce, setTotalLivesForce] = useState(0);
+  const [name, setName] = useState('Введите Ваше имя')
+  const [lives, setLives] = useState([])
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [newLife, setNewLife] = useState({
+    name: '',
+    background: '',
+    force: '',
+  })
+  const [totalLivesForce, setTotalLivesForce] = useState(0)
 
-useEffect(() => {
-  setTotalLives();
-}, [lives]); 
+  useEffect(() => {
+    setTotalLives()
+  }, [lives])
 
   const openModal = () => {
-    setModalIsOpen(true);
-  };
+    setModalIsOpen(true)
+  }
 
   const closeModal = () => {
-    setModalIsOpen(false);
+    setModalIsOpen(false)
     // Очистите данные для следующего использования
-    setNewLife({ name: '', background: '', force: '' });
-    
-  };
+    setNewLife({ name: '', background: '', force: '' })
+  }
 
   const handleAddLife = () => {
     // Добавьте новую жизнь в массив
-    setLives([...lives, newLife]);
-    closeModal();
-    
-  };
+    setLives([...lives, newLife])
+    closeModal()
+  }
 
   const handleInputChange = (e) => {
     // Обновите состояние новой жизни при изменении ввода
-    setNewLife({ ...newLife, [e.target.name]: e.target.value });
-   
-  };
-
-
+    setNewLife({ ...newLife, [e.target.name]: e.target.value })
+  }
 
   function setTotalLives() {
-    let result = 0;
-    lives.forEach(element => {
-      result = parseFloat(element.force) + result;
+    let result = 0
+    lives.forEach((element) => {
+      result = parseFloat(element.force) + result
       console.log(totalLivesForce, lives.length)
-    });
-    return setTotalLivesForce(result / lives.length);
+    })
+    return setTotalLivesForce(result / lives.length)
   }
 
   return (
     <>
-     <div className='content'>
-  <header className='header'>
-    <div className='header__logo'></div>
-    <input onChange={(evt)=> setName(evt.target.value)} className='header__input' placeholder={name} value={name} />
-  </header>
-  <main className='main'>
-  <div className='live__result'>
-      <div className='live__level-total' style={{ width: `${(totalLivesForce / 10) * 100}%` }}></div>
-    </div>
-    <h2 className='title'>
-            {lives.length === 0
-              ?
-              "Добавьте Ваши жизни"
-              :
-              "Ваши  " + lives.length + "  жизни"}
-          </h2>
-    <section className='lives'>
-      <ul className='lives__list'>
-        {lives.map((item, index) => (
-        <div key={index} className='live'>
-          <img className='live__img' src={item.background} alt={item.name}/>
-          <p className='live__name'>{item.name}</p>
-          <div className='live__level'>
-            <div className='live__level-fill' style={{ width: `${(item.force / 10) * 100}%` }}></div>
+      <div className="content">
+        <header className="header">
+          <div className="header__logo"></div>
+          <input
+            onChange={(evt) => setName(evt.target.value)}
+            className="header__input"
+            placeholder={name}
+            value={name}
+          />
+        </header>
+        <main className="main">
+          <div className="live__result">
+            <div
+              className="live__level-total"
+              style={{ width: `${(totalLivesForce / 10) * 100}%` }}
+            ></div>
           </div>
-        </div>
-        ))}
-      </ul>
-      <button onClick={openModal} className='lives__add'>
-        Добавить жизнь
-      </button>
-    </section>
-  </main>
-</div>
-<Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel='Add Life Modal'>
-  <h2>Добавить жизнь</h2>
-  <label>
-    Название:
-    <input type='text' name='name' value={newLife.name} onChange={handleInputChange} />
-  </label>
-  <label>
-    Фон (URL изображения):
-    <input type='text' name='background' value={newLife.background} onChange={handleInputChange} />
-  </label>
-  <label>
-    Оцените жизнь от 1 до 10:
-    <input type='text' name='force' value={newLife.force} onChange={handleInputChange} />
-  </label>
-  <button onClick={handleAddLife}>Добавить</button>
-  <button onClick={closeModal}>Отмена</button>
-</Modal>
+          <h2 className="title">
+            {lives.length === 0
+              ? 'Добавьте Ваши жизни'
+              : 'Ваши  ' + lives.length + '  жизни'}
+          </h2>
+          <section className="lives">
+            <ul className="lives__list">
+              {lives.map((item, index) => (
+                <div key={index} className="live">
+                  <img
+                    className="live__img"
+                    src={item.background}
+                    alt={item.name}
+                  />
+                  <p className="live__name">{item.name}</p>
+                  <div className="live__level">
+                    <div
+                      className="live__level-fill"
+                      style={{ width: `${(item.force / 10) * 100}%` }}
+                    ></div>
+                  </div>
+                </div>
+              ))}
+            </ul>
+            <button onClick={openModal} className="lives__add">
+              Добавить жизнь
+            </button>
+          </section>
+        </main>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Add Life Modal"
+      >
+        <h2>Добавить жизнь</h2>
+        <label>
+          Название:
+          <input
+            type="text"
+            name="name"
+            value={newLife.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Фон (URL изображения):
+          <input
+            type="text"
+            name="background"
+            value={newLife.background}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Оцените жизнь от 1 до 10:
+          <input
+            type="text"
+            name="force"
+            value={newLife.force}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button onClick={handleAddLife}>Добавить</button>
+        <button onClick={closeModal}>Отмена</button>
+      </Modal>
     </>
   )
 }
--- schedule vite/src/Page.jsx
+++ schedule vite/src/Page.jsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-
 function Page(props) {
- 
-
   return (
     <>
-   <div className='page'>
-    <h2 className='page__title'>{props.name}</h2>
-    <img className='page__img' src={props.background} alt={props.name}></img>
-    <div className='live__level'>
-      <div className='live__level-fill' style={{ width: `${(props.force / 10) * 100}%` }}></div>
-    </div>
-   </div>
+      <div className="page">
+        <h2 className="page__title">{props.name}</h2>
+        <img
+          className="page__img"
+          src={props.background}
+          alt={props.name}
+        ></img>
+        <div className="live__level">
+          <div
+            className="live__level-fill"
+            style={{ width: `${(props.force / 10) * 100}%` }}
+          ></div>
+        </div>
+      </div>
     </>
   )
 }
